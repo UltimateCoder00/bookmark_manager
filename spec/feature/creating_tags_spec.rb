67b9eea5725502ec7a8f6af57c404d041fc21cae
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-05 11:55:06 +0100

    tagging links

--- spec/feature/creating_tags_spec.rb
+++ spec/feature/creating_tags_spec.rb
@@ -1,11 +1,11 @@
-feature 'Creating new tags' do
-  scenario 'I can create a tag' do
+feature 'Adding tags' do
+  scenario 'I can add a single tag to a new link' do
     visit('links/new')
     fill_in 'title', with: "Google"
     fill_in 'url', with: "https://www.google.com"
-    fill_in 'tag', with: "Search engine"
+    fill_in 'tags', with: "Search engine"
     click_button 'Add link'
-
-    expect(page).to have_content ("Search engine")
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('Search engine')
   end
 end
