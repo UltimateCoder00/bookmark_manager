80cd8040195c9e9c0cb4e0b0bebed5c26be8df04
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-07 15:37:48 +0100

    Preventing duplicate registrations

--- spec/feature/user_management_spec.rb
+++ spec/feature/user_management_spec.rb
@@ -9,14 +9,24 @@ feature 'User sign up' do
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq '/users'
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario "I can't sign up without an email address" do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Email must not be blank'
   end
 
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: 'test@test') }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Email has an invalid format'
+  end
+
+  scenario "I can't sign up with an already registered email address" do
+    sign_up
+    expect { sign_up }.not_to change(User, :count)
+    expect(page).to have_content 'Email is already taken'
   end
 end
