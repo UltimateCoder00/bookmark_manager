1ab1baef4848fdbbe9f01daf012666b5252df2ca
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-07 10:13:33 +0100

    Password confirmation

--- spec/feature/user_management_spec.rb
+++ spec/feature/user_management_spec.rb
@@ -5,4 +5,8 @@ feature 'User sign up' do
     expect(page).to have_content "Welcome, test@test.com"
     expect(User.first.email).to eq("test@test.com")
   end
+
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,8 +1,7 @@
-def sign_up
-  visit('/users/new')
-  expect(page.status_code).to eq(200)
-  fill_in 'email', with: "test@test.com"
-  fill_in 'password', with: "password"
+def sign_up(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+  visit '/users/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
-
 end
