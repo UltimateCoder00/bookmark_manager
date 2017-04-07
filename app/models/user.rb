270c59bdd66f966c2681b98993a74d83c985b50e
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-07 12:19:24 +0100

    Add format requirements to email_address

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User
   validates_format_of :email, as: :email_address
 
   property :id,               Serial
-  property :email,            String, :required => true
+  property :email,            String, format: :email_address, :required => true
   property :password_digest,  Text
 
   def password=(password)
